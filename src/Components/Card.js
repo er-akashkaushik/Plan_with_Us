14cfc783428e785de9f39b5212f30a1dc4d41221
Akash Kaushik <akashkaushik.er@gmail.com>
2024-06-19 01:01:22 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -12,7 +12,7 @@ function Card({id, image, info, price, name, removeTour}){
 return (
 
     <div className="card">
-        <img src={image} className="image"></img>
+        <img src={image} className="image" alt= "Not-found"></img>
         
         <div className="tour-info">    
             <div className="tour-details">
